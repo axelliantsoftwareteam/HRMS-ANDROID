124c732b00c452b406169d0455f5be335d83485b
arslan <arslan.umar@khazanapk.com>
2022-10-03 20:11:16 +0500

    Fix Microsoft Calender

--- HRM Android App/app/src/main/java/com/example/hrm/Interface/OnItemClick.java
+++
@@ -1,6 +0,0 @@
-package com.example.hrm.Interface;
-
-public interface OnItemClick
-{
-    void onClick (String value);
-}
--- HRM Android App/app/src/main/java/com/example/hrm/Interface/RecyclerViewDataPass.java
+++
@@ -1,5 +0,0 @@
-package com.example.hrm.Interface;
-
-public interface RecyclerViewDataPass {
-    public void pass(String productName, String quantity);
-}
